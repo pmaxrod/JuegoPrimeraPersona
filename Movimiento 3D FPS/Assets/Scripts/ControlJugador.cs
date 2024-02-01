588f749c0e25eaeb5f0f21d8600aaebbe0f3de89
Pablo Maxiá <pmaxiarodriguez@gmail.com>
2024-02-01 17:48:36 +0100

    Primer Commit Movimiento 3D FPS

---
+++ Movimiento 3D FPS/Assets/Scripts/ControlJugador.cs
@@ -0,0 +1,100 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class ControlJugador : MonoBehaviour
+{
+    private Vector2 ratonDelta;
+
+    [Header("Vista Cámara")]
+    public float sensibilidadRaton;
+    public float maxVistaX;
+    public float minVistaX;
+    public Transform camara;
+    private float rotacionActualCamara;
+
+    [Header("Movimiento")]
+    public float velocidadMovimiento;
+    public float fuerzaSalto;
+    private Vector2 movimientoActualEntrada;
+    private Rigidbody fisica;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    private void LateUpdate()
+    {
+        VistaCamara();
+    }
+
+    private void Awake()
+    {
+        fisica = gameObject.GetComponent<Rigidbody>();
+    }
+
+    private void FixedUpdate()
+    {
+        Movimiento();
+    }
+
+    public void OnMirarInput(InputAction.CallbackContext context)
+    {
+        ratonDelta = context.ReadValue<Vector2>();
+    }
+
+    public void OnMovimientoInput(InputAction.CallbackContext context)
+    {
+        if (context.phase == InputActionPhase.Performed)
+        {
+            movimientoActualEntrada = context.ReadValue<Vector2>();
+        }
+        else if (context.phase == InputActionPhase.Canceled)
+        {
+            movimientoActualEntrada = Vector2.zero;
+        }
+    }
+
+    public void OnSaltoInput(InputAction.CallbackContext context)
+    {
+//        saltoEntrada = context.ReadValue<Vector2>();
+        Salto();
+    }
+
+    private void VistaCamara()
+    {
+        rotacionActualCamara += ratonDelta.y * sensibilidadRaton;
+        rotacionActualCamara = Mathf.Clamp(rotacionActualCamara,
+        minVistaX, maxVistaX);
+        camara.transform.localRotation = Quaternion.Euler(-rotacionActualCamara, 0, 0);
+        transform.eulerAngles += Vector3.up * ratonDelta.x * sensibilidadRaton;
+    }
+
+    private void Movimiento()
+    {
+        Vector3 direccion = transform.forward * movimientoActualEntrada.y + transform.right * movimientoActualEntrada.x;
+        direccion *= velocidadMovimiento;
+        direccion.y = fisica.velocity.y;
+        fisica.velocity = direccion;
+    }
+
+    private void Salto()
+    {
+        Ray rayo = new Ray(transform.position, Vector3.down);
+
+        if (Physics.Raycast(rayo, 1.0f))
+        {
+            fisica.AddForce(Vector3.up * fuerzaSalto, ForceMode.Impulse);
+        }
+    }
+
+
+}
